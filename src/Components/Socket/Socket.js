616c95497af9278689e52e2ca4eb88567644d421
Aleem Ilyas <aleemempire215@gmail.com>
2024-02-14 18:43:47 +0500

    update in socket js for cors

--- src/Components/Socket/Socket.js
+++ src/Components/Socket/Socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://talky-backend.vercel.app/';
+const URL = 'https://talky-backend.vercel.app';
 
 export const socket = io(URL, {
     withCredentials: true,
