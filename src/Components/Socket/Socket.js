09b3947f513567f925bf18038936f854500a1876
Aleem Ilyas <aleemempire215@gmail.com>
2024-02-14 18:45:17 +0500

    update in socket js for cors

--- src/Components/Socket/Socket.js
+++ src/Components/Socket/Socket.js
@@ -4,7 +4,6 @@ import { io } from 'socket.io-client';
 const URL = 'https://talky-backend.vercel.app';
 
 export const socket = io(URL, {
-    withCredentials: true,
     extraHeaders: {
         "Access-Control-Allow-Origin": "https://talky-gules.vercel.app",
         "authorization": localStorage.getItem("token") || ""
